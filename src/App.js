591e20610cea06a914457e2ca8d33314f769b9f8
Shaban <mohammad.shaban@u2opiamobile.com>
2017-04-25 22:12:28 +0530

    readme.md changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React                from 'react'
 import DaysTimer        from 'components/DaysTimer'
 class App extends React.Component {
   render() {
-console.log("dajdjsbjd")
+      let style = {
+          parentDiv:{
+              background: '#ccc',
+              display: 'inline-block'
+          },
+          value:{
+              display: 'inline-block',
+              padding: '10px'
+          }
+      }
+      let date = "2018-04-15T16:00:00.000+05:30";
     return (
-      <DaysTimer 
-	startDate={"2017-04-15T16:00:00.000+05:30"}
-	/>
+      <DaysTimer
+          style={style}
+          endDate={date}
+      />
         
     )
   }
